1aca9e5dc06a1ba6368b892b3369f764aace9353
Joniel <57368054+jonielbento@users.noreply.github.com>
2024-08-07 22:11:45 -0300

    Alterações get all

--- fiap_5nett_tech.Application/DataTransfer/Request/GetAllContactRequest.cs
+++ fiap_5nett_tech.Application/DataTransfer/Request/GetAllContactRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace fiap_5nett_tech.Application.DataTransfer.Request;
 
-public class GetAllContactRequest : ContactRequest
+public class GetAllContactRequest : ContactRequestAll
 {
     public int PageNumber { get; set; } = Configuration.DefaultPageNumber;
     public int PageSize { get; set; } = Configuration.DefaultPageSize;
